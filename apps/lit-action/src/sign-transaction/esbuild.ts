7182d56a13da0899b8c95048876ece72629be93f
imajindev <dev@majin.land>
2024-09-05 13:35:19 +0800

    refactor: reuse esbuild-shims.ts

--- apps/lit-action/src/sign-transaction/esbuild.ts
+++ apps/lit-action/src/sign-transaction/esbuild.ts
@@ -1,26 +1,26 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.23.0/mod.js";
-import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.5/mod.ts";
+import * as esbuild from 'https://deno.land/x/esbuild@v0.23.0/mod.js'
+import { denoPlugins } from 'https://deno.land/x/esbuild_deno_loader@0.8.5/mod.ts'
 
 const go = async () => {
   let result = await esbuild.build({
-    entryPoints: ["./src/sign-transaction/index.ts"],
+    entryPoints: ['./src/sign-transaction/index.ts'],
     bundle: true,
     minify: true,
     sourcemap: false,
-    outfile: "./dist/sign-transaction.js",
-    sourceRoot: "./",
-    platform: "node",
+    outfile: './dist/sign-transaction.js',
+    sourceRoot: './',
+    platform: 'node',
     metafile: true,
-    external: ["ethers"],
-    inject: ["./src/sign-transaction/esbuild-shims.ts"],
+    external: ['ethers'],
+    inject: ['./src/esbuild-shims.ts'],
     plugins: denoPlugins(),
-  });
+  })
 
   // Uncomment this if you want to analyze the metafile
-  let text = await esbuild.analyzeMetafile(result.metafile);
-  console.log(text);
+  let text = await esbuild.analyzeMetafile(result.metafile)
+  console.log(text)
 
-  console.log("Build completed successfully");
-};
+  console.log('Build completed successfully')
+}
 
-go();
+go()
